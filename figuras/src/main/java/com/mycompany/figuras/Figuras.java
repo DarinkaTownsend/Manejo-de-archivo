33c457092008728d03a80b13ffa6c7dac43edffc
FiorellaQuijano <fioanqui@espol.edu.ec>
2023-07-17 09:58:29 -0500

    Comentario
    comentario

--- figuras/src/main/java/com/mycompany/figuras/Figuras.java
+++ figuras/src/main/java/com/mycompany/figuras/Figuras.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
  * @author darin
  */
 public class Figuras {
-
+    
     public static void main(String[] args) {
         ArrayList<Circulo> circulos=Archivos.lecturaArchivo("info.txt");
         for(Circulo c: circulos){
@@ -20,4 +20,6 @@ public class Figuras {
         
         Archivos.escribirArchivo(circulos, "cirulos.txt");
     }
+    
+    //comentario
 }
